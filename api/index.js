193ca78be2144b6294c757fd77a0331b8d65fb7f
Virginia Montoya <mvirginiamontoya@yahoo.com.ar>
2023-04-28 14:52:10 -0300

    arreglos css

--- api/index.js
+++ api/index.js
@@ -20,12 +20,16 @@
 //Genero la conexion entre el server y la DB
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const {getDataApi} = require('./src/controllers/dataApi.js')
+const {getDataApi} = require('./src/controllers/dataApi.js');
+require('dotenv').config();
+const { PORT } = process.env;
+
+const port = PORT || 3001;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(async () => {
   await getDataApi()
-  server.listen(3001, () => {
+  server.listen(port, () => {
     console.log('%s listening at 3001', server.name); // eslint-disable-line no-console
     console.log('connected to the database');
   });
